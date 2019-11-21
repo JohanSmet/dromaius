24835f36764990aa59813916731f40c93bf29f2e
Johan Smet <johan.smet@justcode.be>
2019-11-21 16:29:40 +0100

    gui: start UI program

---
+++ src/gui/main_gui.c
@@ -0,0 +1,13 @@
+// gui/main_gui.c - Johan Smet - BSD-3-Clause (see LICENSE)
+
+#include <nuklear/nuklear.h>
+
+char *nuklear_config_window_title(void) {
+	return "Dromaius";
+}
+
+void nuklear_on_start(struct nk_context *ctx) {
+}
+
+void nuklear_gui(struct nk_context *ctx) {
+}
