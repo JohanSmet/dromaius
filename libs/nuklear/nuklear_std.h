9816d19f49bdb6a6b6589dd3f9b2fa3ad1d2c9cc
Johan Smet <johan.smet@justcode.be>
2019-11-25 23:06:04 +0100

    nuklear: an easy-to-include header with the required defines enabled

---
+++ libs/nuklear/nuklear_std.h
@@ -0,0 +1,10 @@
+// nuklear_std.h - Johan Smet - BSD-3-Clause (see LICENSE)
+
+#ifndef DROMAIUS_NUKLEAR_STD_H
+#define DROMAIUS_NUKLEAR_STD_H
+
+#define NK_INCLUDE_STANDARD_VARARGS
+#include <stdarg.h>
+#include "nuklear.h"
+
+#endif // DROMAIUS_NUKLEAR_STD_H
