5f7e59ccc3c78cbaa3cc86f9653924b7c9cda12a
Johan Smet <johan.smet@justcode.be>
2019-11-29 19:02:33 +0100

    utils: add function to list files in a directory

--- src/utils.c
+++ src/utils.c
@@ -2,10 +2,12 @@
 
 #include "utils.h"
 #include "stb/stb_ds.h"
+#include <cute/cute_files.h>
 
 #include <assert.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <string.h>
 
 size_t file_load_binary_fixed(const char *filename, uint8_t *buffer, size_t max_len) {
 	assert(filename);
@@ -20,6 +22,28 @@ size_t file_load_binary_fixed(const char *filename, uint8_t *buffer, size_t max_
 	fclose(fp);
 
 	return read;
+}
+
+void dir_list_files(const char *path, const char *ext, const char *prefix, const char ***file_list) {
+
+	cf_dir_t dir;
+
+	for (cf_dir_open(&dir, path); dir.has_next != 0; cf_dir_next(&dir)) {
+		cf_file_t file;
+		cf_read_file(&dir, &file);
+
+		// skip anything that isn't a regular file with the wanted extension
+		if (file.is_reg == 0 || cf_match_ext(&file, ext) == 0) {
+			continue;
+		}
+
+		// optionally check prefix
+		if (prefix && strncmp(file.name, prefix, strlen(prefix)) != 0) {
+			continue;
+		}
+
+		arrpush(*file_list, strdup(file.name));
+	}
 }
 
 char *arr__printf(char *array, const char *fmt, ...) {
--- src/utils.h
+++ src/utils.h
@@ -7,7 +7,10 @@
 
 size_t file_load_binary_fixed(const char *filename, uint8_t *buffer, size_t max_len);
 
+void dir_list_files(const char *path, const char *ext, const char *prefix, const char ***file_list);
+
 char *arr__printf(char *array, const char *fmt, ...);
 #define arr_printf(a, ...) (a) = arr__printf((a), __VA_ARGS__)
+
 
 #endif // DROMAIUS_UTILS_H
