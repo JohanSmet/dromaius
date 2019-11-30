c3abcd478799b8358855e19d2bc576b7364ec3e8
Johan Smet <johan.smet@justcode.be>
2019-11-30 21:54:54 +0100

    gui/panel_clock: allow the target frequency to be changed

--- src/gui/panel_clock.c
+++ src/gui/panel_clock.c
@@ -27,24 +27,44 @@ void panel_clock(struct nk_context *nk_ctx, Clock *clock, struct nk_vec2 pos) {
 	const struct nk_rect panel_bounds = {
 		.x = pos.x,
 		.y = pos.y,
-		.w = 230,
-		.h = 120
+		.w = 300,
+		.h = 125
 	};
+	static const char *units[] = {"Hz", "KHz", "MHz"};
+	static const int factor[] = {1, 1000, 1000000};
 	static const nk_flags panel_flags = NK_WINDOW_BORDER | NK_WINDOW_MOVABLE | NK_WINDOW_SCALABLE | NK_WINDOW_TITLE | NK_WINDOW_NO_SCROLLBAR;
 
 	if (nk_begin(nk_ctx, panel_title, panel_bounds, panel_flags)) {
+
+		// configured frequency
+		int unit_idx = 0;
+		float value = clock->conf_frequency;
+
+		if (value >= 1000000) {
+			value /= 1000000;
+			unit_idx = 2;
+		} else if (value >= 1000) {
+			value /= 1000;
+			unit_idx = 1;
+		}
+
+		nk_layout_row(nk_ctx, NK_STATIC, 25, 2, (float[]) {200, 74});
+		nk_property_float(nk_ctx, "Target frequency", 1, &value, 999, 1, 1);
+		nk_combobox(nk_ctx, units, NK_LEN(units), &unit_idx, 25, nk_vec2(60,200));
+
+		if (clock->conf_frequency != value * factor[unit_idx]) {
+			clock_set_frequency(clock, value * factor[unit_idx]);
+		}
+
+		// real frequency
+		ui_frequency(nk_ctx, "Real frequency: ", clock->real_frequency);
+
 		// state
 		nk_layout_row_begin(nk_ctx, NK_STATIC, 14, 2);
 		nk_layout_row_push(nk_ctx, 128);
 		nk_label(nk_ctx, "Output: ", NK_TEXT_RIGHT);
 		nk_layout_row_push(nk_ctx, 32);
 		nk_label(nk_ctx, (clock->pin_clock) ? "High" : "Low", NK_TEXT_LEFT);
-
-		// configured frequency
-		ui_frequency(nk_ctx, "Target frequency: ", clock->conf_frequency);
-
-		// real frequency
-		ui_frequency(nk_ctx, "Real frequency: ", clock->real_frequency);
 
 		// cycle count
 		nk_layout_row_begin(nk_ctx, NK_STATIC, 14, 2);
