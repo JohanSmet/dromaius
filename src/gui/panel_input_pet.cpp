a595d2f3b20d623628784645d8c2a4484542d126
Johan Smet <johan.smet@justcode.be>
2020-04-19 22:06:40 +0200

    gui - input_pet: support keyboard input and make shift lock work

--- src/gui/panel_input_pet.cpp
+++ src/gui/panel_input_pet.cpp
@@ -5,6 +5,7 @@
 #include "imgui_ex.h"
 
 #include "input_keypad.h"
+#include "GLFW/glfw3.h"
 
 class PanelInputPet : public Panel {
 public:
@@ -34,17 +35,20 @@ public:
 			for (size_t r = 0; r < 10; ++r) {
 				for (size_t c = 0; c < 8; ++c) {
 					size_t k = (r * keypad->col_count) + c;
-					if (key_positions[k].width == 0) {
+					auto &key = key_positions[k];
+
+					if (key.width == 0) {
 						continue;
 					}
 
-					ImVec2 pos = {origin.x + key_positions[k].left, origin.y + key_positions[k].top};
+					ImVec2 pos = {origin.x + key.left, origin.y + key.top};
 					ImGui::SetCursorScreenPos(pos);
 
 					ImGui::PushID((int) k);
-					ImGuiEx::RectFilled({pos.x + 1, pos.y+1}, {pos.x + key_positions[k].width - 2, pos.y + KEY_SIZE - 2}, colors[keypad->keys[k]]);
-					ImGuiEx::Text(labels[k].c_str(), {key_positions[k].width, KEY_SIZE}, ImGuiEx::TAH_CENTER , ImGuiEx::TAV_CENTER);
-					if (ImGui::InvisibleButton(labels[k].c_str(), {key_positions[k].width, KEY_SIZE})) {
+					ImGuiEx::RectFilled({pos.x + 1, pos.y+1}, {pos.x + key.width - 2, pos.y + KEY_SIZE - 2}, colors[keypad->keys[k]]);
+					ImGuiEx::Text(labels[k].c_str(), {key.width, KEY_SIZE}, ImGuiEx::TAH_CENTER , ImGuiEx::TAV_CENTER);
+					if (ImGui::InvisibleButton(labels[k].c_str(), {key.width, KEY_SIZE}) ||
+				        (key.key_code != 0 && ImGui::GetIO().KeysDown[key.key_code])) {
 						input_keypad_key_pressed(keypad, r, c);
 					}
 					ImGui::PopID();
@@ -59,8 +63,13 @@ public:
 			ImGuiEx::RectFilled({pos.x + 1, pos.y + 1}, {pos.x + size.x - 2, pos.y + size.y - 2}, colors[shift_locked]);
 			ImGuiEx::Text("Shift\nLock", size, ImGuiEx::TAH_CENTER, ImGuiEx::TAV_CENTER);
 
-			if (ImGui::InvisibleButton("ShiftLock", size)) {
+			if (ImGui::InvisibleButton("ShiftLock", size) ||
+				ImGui::IsKeyPressed(GLFW_KEY_CAPS_LOCK)) {
 				shift_locked = !shift_locked;
+			}
+
+			if (shift_locked) {
+				input_keypad_key_pressed(keypad, KEY_SHIFT_R, KEY_SHIFT_C);
 			}
 
 			ImGui::EndChild();
@@ -91,110 +100,113 @@ private:
 		float left;
 		float top;
 		float width;
+		int   key_code;
 	};
 
 	constexpr static float KEY_SIZE = 32;
+	constexpr static int KEY_SHIFT_R = 8;
+	constexpr static int KEY_SHIFT_C = 0;
 
 	constexpr static KeyPos key_positions[80] = {
 		// row 0
-		{1*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 0 (!)
-		{3*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 1 (#)
-		{5*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 2 (%)
-		{7*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 3 (&)
-		{9*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 4 (()
-		{11*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 5 (<-)
-		{500,			0*KEY_SIZE, KEY_SIZE},			// column 6 (^H)
-		{500+2*KEY_SIZE,0*KEY_SIZE, KEY_SIZE},			// column 7 (Cursor Right)
+		{1*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_1},			// column 0 (!)
+		{3*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_3},			// column 1 (#)
+		{5*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_5},			// column 2 (%)
+		{7*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_7},			// column 3 (&)
+		{9*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_9},			// column 4 (()
+		{11*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_MINUS},		// column 5 (<-)
+		{500,			0*KEY_SIZE, KEY_SIZE, GLFW_KEY_BACKSPACE},	// column 6 (^H)
+		{500+2*KEY_SIZE,0*KEY_SIZE, KEY_SIZE, GLFW_KEY_RIGHT},		// column 7 (Cursor Right)
 
 		// row 1
-		{2*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 0 (")
-		{4*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 1 ($)
-		{6*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 2 (')
-		{8*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 3 (\)
-		{10*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 4 ())
-		{0,				0,			0},					// column 5 (not used)
-		{500+1*KEY_SIZE,0*KEY_SIZE, KEY_SIZE},			// column 6 (cursor down)
-		{500+3*KEY_SIZE,0*KEY_SIZE, KEY_SIZE},			// column 7 (DEL)
+		{2*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_2},			// column 0 (")
+		{4*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_4},			// column 1 ($)
+		{6*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_6},			// column 2 (')
+		{8*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_8},			// column 3 (\)
+		{10*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_0},			// column 4 ())
+		{0,				0,			0,	   	  0},					// column 5 (not used)
+		{500+1*KEY_SIZE,0*KEY_SIZE, KEY_SIZE, GLFW_KEY_DOWN},		// column 6 (cursor down)
+		{500+3*KEY_SIZE,0*KEY_SIZE, KEY_SIZE, GLFW_KEY_DELETE},		// column 7 (DEL)
 
 		// row 2
-		{1.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE},			// column 0 (q)
-		{3.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE},			// column 1 (e)
-		{5.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE},			// column 2 (t)
-		{7.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE},			// column 3 (u)
-		{9.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE},			// column 4 (o)
-		{11.5f*KEY_SIZE,1*KEY_SIZE, KEY_SIZE},			// column 5 (UP)
-		{500+0*KEY_SIZE,1*KEY_SIZE, KEY_SIZE},			// column 6 (7)
-		{500+2*KEY_SIZE,1*KEY_SIZE, KEY_SIZE},			// column 7 (9)
+		{1.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE, GLFW_KEY_Q},			// column 0 (q)
+		{3.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE, GLFW_KEY_E},			// column 1 (e)
+		{5.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE, GLFW_KEY_T},			// column 2 (t)
+		{7.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE, GLFW_KEY_U},			// column 3 (u)
+		{9.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE, GLFW_KEY_O},			// column 4 (o)
+		{11.5f*KEY_SIZE,1*KEY_SIZE, KEY_SIZE, GLFW_KEY_UP},			// column 5 (UP)
+		{500+0*KEY_SIZE,1*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_7},		// column 6 (7)
+		{500+2*KEY_SIZE,1*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_9},		// column 7 (9)
 
 		// row 3
-		{2.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE},			// column 0 (w)
-		{4.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE},			// column 1 (r)
-		{6.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE},			// column 2 (y)
-		{8.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE},			// column 3 (i)
-		{10.5f*KEY_SIZE,1*KEY_SIZE, KEY_SIZE},			// column 4 (p)
-		{0,				0,			0},					// column 5 (not used)
-		{500+1*KEY_SIZE,1*KEY_SIZE, KEY_SIZE},			// column 6 (8)
-		{500+3*KEY_SIZE,1*KEY_SIZE, KEY_SIZE},			// column 7 (/)
+		{2.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE, GLFW_KEY_W},			// column 0 (w)
+		{4.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE, GLFW_KEY_R},			// column 1 (r)
+		{6.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE, GLFW_KEY_Y},			// column 2 (y)
+		{8.5f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE, GLFW_KEY_I},			// column 3 (i)
+		{10.5f*KEY_SIZE,1*KEY_SIZE, KEY_SIZE, GLFW_KEY_P},			// column 4 (p)
+		{0,				0,			0,		  0},					// column 5 (not used)
+		{500+1*KEY_SIZE,1*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_8},		// column 6 (8)
+		{500+3*KEY_SIZE,1*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_DIVIDE},	// column 7 (/)
 
 		// row 4
-		{1.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE},			// column 0 (a)
-		{3.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE},			// column 1 (d)
-		{5.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE},			// column 2 (g)
-		{7.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE},			// column 3 (j)
-		{9.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE},			// column 4 (l)
-		{0,				0,			0},					// column 5 (not used)
-		{500+0*KEY_SIZE,2*KEY_SIZE, KEY_SIZE},			// column 6 (4)
-		{500+2*KEY_SIZE,2*KEY_SIZE, KEY_SIZE},			// column 7 (6)
+		{1.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE, GLFW_KEY_A},			// column 0 (a)
+		{3.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE, GLFW_KEY_D},			// column 1 (d)
+		{5.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE, GLFW_KEY_G},			// column 2 (g)
+		{7.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE, GLFW_KEY_J},			// column 3 (j)
+		{9.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE, GLFW_KEY_L},			// column 4 (l)
+		{0,				0,			0,		  0},					// column 5 (not used)
+		{500+0*KEY_SIZE,2*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_4},		// column 6 (4)
+		{500+2*KEY_SIZE,2*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_6},		// column 7 (6)
 
 		// row 5
-		{2.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE},			// column 0 (s)
-		{4.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE},			// column 1 (f)
-		{6.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE},			// column 2 (h)
-		{8.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE},			// column 3 (k)
-		{10.7f*KEY_SIZE,2*KEY_SIZE, KEY_SIZE},			// column 4 (:)
-		{0,				0,			0},					// column 5 (not used)
-		{500+1*KEY_SIZE,2*KEY_SIZE, KEY_SIZE},			// column 6 (5)
-		{500+3*KEY_SIZE,2*KEY_SIZE, KEY_SIZE},			// column 7 (*)
+		{2.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE, GLFW_KEY_S},			// column 0 (s)
+		{4.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE, GLFW_KEY_F},			// column 1 (f)
+		{6.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE, GLFW_KEY_H},			// column 2 (h)
+		{8.7f*KEY_SIZE,	2*KEY_SIZE, KEY_SIZE, GLFW_KEY_K},			// column 3 (k)
+		{10.7f*KEY_SIZE,2*KEY_SIZE, KEY_SIZE, GLFW_KEY_SEMICOLON},	// column 4 (:)
+		{0,				0,			0,		  0},					// column 5 (not used)
+		{500+1*KEY_SIZE,2*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_5},		// column 6 (5)
+		{500+3*KEY_SIZE,2*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_MULTIPLY},// column 7 (*)
 
 		// row 6
-		{2.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE},			// column 0 (z)
-		{4.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE},			// column 1 (c)
-		{6.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE},			// column 2 (b)
-		{8.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE},			// column 3 (m)
-		{10.2f*KEY_SIZE,3*KEY_SIZE, KEY_SIZE},			// column 4 (;)
-		{12.7f*KEY_SIZE,2*KEY_SIZE, KEY_SIZE*1.5f},		// column 5 (RETURN)
-		{500+0*KEY_SIZE,3*KEY_SIZE, KEY_SIZE},			// column 6 (1)
-		{500+2*KEY_SIZE,3*KEY_SIZE, KEY_SIZE},			// column 7 (3)
+		{2.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE, GLFW_KEY_Z},			// column 0 (z)
+		{4.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE, GLFW_KEY_C},			// column 1 (c)
+		{6.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE, GLFW_KEY_B},			// column 2 (b)
+		{8.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE, GLFW_KEY_M},			// column 3 (m)
+		{10.2f*KEY_SIZE,3*KEY_SIZE, KEY_SIZE, GLFW_KEY_PERIOD},		// column 4 (;)
+		{12.7f*KEY_SIZE,2*KEY_SIZE, KEY_SIZE*1.5f, GLFW_KEY_ENTER},	// column 5 (RETURN)
+		{500+0*KEY_SIZE,3*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_1},		// column 6 (1)
+		{500+2*KEY_SIZE,3*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_7},		// column 7 (3)
 
 		// row 7
-		{3.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE},			// column 0 (x)
-		{5.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE},			// column 1 (v)
-		{7.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE},			// column 2 (n)
-		{9.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE},			// column 3 (,)
-		{11.2f*KEY_SIZE,3*KEY_SIZE, KEY_SIZE},			// column 4 (?)
-		{0,				0,			0},					// column 5 (not used)
-		{500+1*KEY_SIZE,3*KEY_SIZE, KEY_SIZE},			// column 6 (2)
-		{500+3*KEY_SIZE,3*KEY_SIZE, KEY_SIZE},			// column 7 (+)
+		{3.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE, GLFW_KEY_X},			// column 0 (x)
+		{5.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE, GLFW_KEY_V},			// column 1 (v)
+		{7.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE, GLFW_KEY_N},			// column 2 (n)
+		{9.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE, GLFW_KEY_COMMA},		// column 3 (,)
+		{11.2f*KEY_SIZE,3*KEY_SIZE, KEY_SIZE, GLFW_KEY_SLASH},		// column 4 (?)
+		{0,				0,			0,		  0},					// column 5 (not used)
+		{500+1*KEY_SIZE,3*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_2},		// column 6 (2)
+		{500+3*KEY_SIZE,3*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_ADD},		// column 7 (+)
 
 		// row 8
-		{0.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE*2.0f},		// column 0 (SHIFT)
-		{0.0f*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE},			// column 1 (@)
-		{13.0f*KEY_SIZE,0*KEY_SIZE, KEY_SIZE},			// column 2 (])
-		{0,				0,			0},					// column 3 (not used)
-		{13.5f*KEY_SIZE,1*KEY_SIZE, KEY_SIZE},			// column 4 (>)
-		{12.2f*KEY_SIZE,3*KEY_SIZE,	KEY_SIZE*2.0f},		// column 5 (SHIFT)
-		{500+0*KEY_SIZE,4*KEY_SIZE, KEY_SIZE},			// column 6 (0)
-		{500+2*KEY_SIZE,4*KEY_SIZE, KEY_SIZE},			// column 7 (-)
+		{0.2f*KEY_SIZE,	3*KEY_SIZE, KEY_SIZE*2.0f, GLFW_KEY_LEFT_SHIFT},	// column 0 (SHIFT)
+		{0.0f*KEY_SIZE,	0*KEY_SIZE, KEY_SIZE, GLFW_KEY_GRAVE_ACCENT},		// column 1 (@)
+		{13.0f*KEY_SIZE,0*KEY_SIZE, KEY_SIZE, GLFW_KEY_RIGHT_BRACKET},		// column 2 (])
+		{0,				0,			0,		  0},							// column 3 (not used)
+		{13.5f*KEY_SIZE,1*KEY_SIZE, KEY_SIZE, GLFW_KEY_BACKSLASH},			// column 4 (>)
+		{12.2f*KEY_SIZE,3*KEY_SIZE,	KEY_SIZE*2.0f, GLFW_KEY_RIGHT_SHIFT},	// column 5 (SHIFT)
+		{500+0*KEY_SIZE,4*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_0},				// column 6 (0)
+		{500+2*KEY_SIZE,4*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_SUBTRACT},		// column 7 (-)
 
 		// row 9
-		{0.0f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE*1.5f},		// column 0 (RVS)
-		{12.0f*KEY_SIZE,0*KEY_SIZE, KEY_SIZE},			// column 1 ([)
-		{1.8f*KEY_SIZE,	4*KEY_SIZE, KEY_SIZE*9.8f},		// column 2 (SPACE)
-		{12.5f*KEY_SIZE,1*KEY_SIZE, KEY_SIZE},			// column 3 (<)
-		{11.7f*KEY_SIZE,2*KEY_SIZE, KEY_SIZE},			// column 4 (STOP)
-		{0,				0,			0},					// column 5 (not used)
-		{500+1*KEY_SIZE,4*KEY_SIZE, KEY_SIZE},			// column 6 (.)
-		{500+3*KEY_SIZE,4*KEY_SIZE, KEY_SIZE},			// column 7 (=)
+		{0.0f*KEY_SIZE,	1*KEY_SIZE, KEY_SIZE*1.5f, GLFW_KEY_TAB},		// column 0 (RVS)
+		{12.0f*KEY_SIZE,0*KEY_SIZE, KEY_SIZE, GLFW_KEY_LEFT_BRACKET},	// column 1 ([)
+		{1.8f*KEY_SIZE,	4*KEY_SIZE, KEY_SIZE*9.8f, GLFW_KEY_SPACE},		// column 2 (SPACE)
+		{12.5f*KEY_SIZE,1*KEY_SIZE, KEY_SIZE, GLFW_KEY_APOSTROPHE},		// column 3 (<)
+		{11.7f*KEY_SIZE,2*KEY_SIZE, KEY_SIZE, GLFW_KEY_ESCAPE},			// column 4 (STOP)
+		{0,				0,			0,		  0},						// column 5 (not used)
+		{500+1*KEY_SIZE,4*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_DECIMAL},		// column 6 (.)
+		{500+3*KEY_SIZE,4*KEY_SIZE, KEY_SIZE, GLFW_KEY_KP_ENTER},		// column 7 (=)
 	};
 
 private:
