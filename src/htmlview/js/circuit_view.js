d2bfcfcd3ab17bdcf6c44a8e5ebe69f87fcec025
Johan Smet <johan.smet@justcode.be>
2020-09-16 21:51:24 +0200

    htmlview: implement zooming and panning the schematics view

--- src/htmlview/js/circuit_view.js
+++ src/htmlview/js/circuit_view.js
@@ -15,8 +15,13 @@ const SCHEMATIC_SHEETS = [
 
 export class CircuitView {
 	svg_document = null;
+	svg_layer = null;
 	svg_load_count = 0;
 	svg_hovered_signal = '';
+
+	svg_pan_active = false;
+	svg_pan_origin = null;
+	svg_pan_transform = null;
 
 	constructor(container) {
 		var tabbar = $('<div/>').attr('class', 'tab');
@@ -61,6 +66,7 @@ export class CircuitView {
 
 		// keep reference to selected svg
 		this.svg_document = $('div' + obj_id + ' svg')[0];
+		this.svg_layer = null;
 	}
 
 	on_wire_mouseover(signal) {
@@ -77,6 +83,66 @@ export class CircuitView {
 		this.on_signal_clicked(signal);
 	}
 
+	event_svg_point(svg, evt) {
+		var p = svg.createSVGPoint();
+		p.x = evt.clientX;
+		p.y = evt.clientY;
+		return p;
+	}
+
+	svg_element_transform(svg_el, transform) {
+		var val = "matrix(" + transform.a + "," + transform.b + "," + transform.c + "," + transform.d + "," + transform.e + "," + transform.f + ")";
+		svg_el.setAttribute("transform", val);
+	}
+
+	svg_transform_layer() {
+		if (this.svg_layer === null) {
+			this.svg_layer = this.svg_document.getElementById('Schematic');
+
+			if (this.svg_document.getAttribute("viewBox")) {
+				this.svg_element_transform(this.svg_layer, this.svg_layer.getCTM());
+				this.svg_document.removeAttribute("viewBox");
+			}
+		}
+
+		return this.svg_layer;
+	}
+
+	on_svg_mousewheel(evt) {
+		var layer = this.svg_transform_layer();
+
+		const scale = 1.0 + evt.deltaY * -0.01;
+		const center = this.event_svg_point(this.svg_document, evt).matrixTransform(layer.getCTM().inverse());
+		const scale_transform = this.svg_document.createSVGMatrix()
+										.translate(center.x, center.y)
+										.scale(scale)
+										.translate(-center.x, -center.y);
+
+		this.svg_element_transform(layer, layer.getCTM().multiply(scale_transform));
+	}
+
+	on_svg_mousedown(evt) {
+		var layer = this.svg_transform_layer();
+
+		this.svg_pan_active = true;
+		this.svg_pan_transform = layer.getCTM().inverse();
+		this.svg_pan_origin = this.event_svg_point(this.svg_document, evt).matrixTransform(this.svg_pan_transform);
+	}
+
+	on_svg_mouseup(evt) {
+		if (this.svg_pan_active) {
+			this.svg_pan_active = false;
+		}
+	}
+
+	on_svg_mousemove(evt) {
+		if (this.svg_pan_active) {
+			var p = this.event_svg_point(this.svg_document, evt).matrixTransform(this.svg_pan_transform);
+			var transform = this.svg_pan_transform.inverse().translate(p.x - this.svg_pan_origin.x, p.y - this.svg_pan_origin.y);
+			this.svg_element_transform(this.svg_layer, transform);
+		}
+	}
+
 	on_svg_loaded(name) {
 
 		// add event handlers to the signal wires
@@ -89,6 +155,13 @@ export class CircuitView {
 			wire.addEventListener('mouseout', () => this.on_wire_mouseout(name));
 			wire.addEventListener('click', () => this.on_wire_mouseclick(name));
 		});
+
+		// add pan & zoom handlers
+		var svg = $("div#" + name + " svg")[0];
+		svg.addEventListener('wheel', (evt) => this.on_svg_mousewheel(evt));
+		svg.addEventListener('mousedown', (evt) => this.on_svg_mousedown(evt));
+		svg.addEventListener('mouseup', (evt) => this.on_svg_mouseup(evt));
+		svg.addEventListener('mousemove', (evt) => this.on_svg_mousemove(evt));
 
 		// if all sheets are loaded: select the first one
 		if (++this.svg_load_count >= SCHEMATIC_SHEETS.length) {
