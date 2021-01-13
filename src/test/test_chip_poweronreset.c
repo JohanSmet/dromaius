8536ab548fe4762c439e8addc6f9827e7fcb4b26
Johan Smet <johan.smet@justcode.be>
2021-01-13 18:26:59 +0100

    poweronreset: add test for trigger functionality

--- src/test/test_chip_poweronreset.c
+++ src/test/test_chip_poweronreset.c
@@ -6,6 +6,7 @@
 
 #define SIGNAL_POOL			por->signal_pool
 #define SIGNAL_COLLECTION	por->signals
+#define SIGNAL_CHIP_ID		por->id
 
 static MunitResult test_reset(const MunitParameter params[], void *user_data_or_fixture) {
 
@@ -37,8 +38,52 @@ static MunitResult test_reset(const MunitParameter params[], void *user_data_or_
 	return MUNIT_OK;
 }
 
+static MunitResult test_trigger(const MunitParameter params[], void *user_data_or_fixture) {
+
+	Simulator *sim = simulator_create(1000);
+	PowerOnReset *por = poweronreset_create(250000, sim, (PowerOnResetSignals) {0});
+
+	SIGNAL_SET_BOOL(trigger_b, true);
+	SIGNAL_SET_BOOL(reset_b, true);
+	signal_pool_cycle(por->signal_pool, sim->current_tick);
+
+	sim->current_tick += 100;
+	por->process(por);
+	munit_assert_false(SIGNAL_NEXT_BOOL(reset_b));
+
+	sim->current_tick += 200;
+	por->process(por);
+	munit_assert_true(SIGNAL_NEXT_BOOL(reset_b));
+
+	sim->current_tick += 50;
+	SIGNAL_SET_BOOL(trigger_b, false);
+	signal_pool_cycle(por->signal_pool, sim->current_tick);
+	por->process(por);
+	munit_assert_false(SIGNAL_NEXT_BOOL(reset_b));
+
+	sim->current_tick += 50;
+	SIGNAL_SET_BOOL(trigger_b, true);
+	signal_pool_cycle(por->signal_pool, sim->current_tick);
+	por->process(por);
+	munit_assert_false(SIGNAL_NEXT_BOOL(reset_b));
+
+	sim->current_tick += 100;
+	por->process(por);
+	munit_assert_false(SIGNAL_NEXT_BOOL(reset_b));
+
+	sim->current_tick += 100;
+	por->process(por);
+	munit_assert_true(SIGNAL_NEXT_BOOL(reset_b));
+
+	sim->current_tick += 50;
+	por->process(por);
+	munit_assert_true(SIGNAL_NEXT_BOOL(reset_b));
+
+	return MUNIT_OK;
+}
 
 MunitTest chip_poweronreset_tests[] = {
 	{ "/reset", test_reset, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+	{ "/trigger", test_trigger, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
 	{ NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
