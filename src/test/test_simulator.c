8f29d961494bb980020966cbfde11d2bbc209ada
Johan Smet <johan.smet@justcode.be>
2021-11-27 16:58:57 +0100

    simulator: add test case for simulator_signal_writers().

--- src/test/test_simulator.c
+++ src/test/test_simulator.c
@@ -3,11 +3,20 @@
 #include "munit/munit.h"
 #include "simulator.h"
 
-#include "stb/stb_ds.h"
+#include "chip_dummy.h"
+
+static void *simulator_setup(const MunitParameter params[], void *user_data) {
+	Simulator *simulator = simulator_create(NS_TO_PS(100));
+	return simulator;
+}
+
+static void simulator_teardown(void *fixture) {
+	simulator_destroy((Simulator *) fixture);
+}
 
 MunitResult test_schedule_event(const MunitParameter params[], void *user_data_or_fixture) {
 
-	Simulator *simulator = simulator_create(NS_TO_PS(100));
+	Simulator *simulator = (Simulator *) user_data_or_fixture;
 
 	munit_assert_ptr_not_null(simulator);
 	munit_assert_ptr_null(simulator->event_schedule);
@@ -51,12 +60,11 @@ MunitResult test_schedule_event(const MunitParameter params[], void *user_data_o
 		munit_assert_ptr_null(third->next);
 	}
 
-	simulator_destroy(simulator);
 	return MUNIT_OK;
 }
 
 MunitResult test_pop_event(const MunitParameter params[], void *user_data_or_fixture) {
-	Simulator *simulator = simulator_create(NS_TO_PS(100));
+	Simulator *simulator = (Simulator *) user_data_or_fixture;
 	munit_assert_ptr_not_null(simulator);
 
 	simulator_schedule_event(simulator, 3, 250);
@@ -79,12 +87,75 @@ MunitResult test_pop_event(const MunitParameter params[], void *user_data_or_fix
 	munit_assert_int32(simulator_pop_scheduled_event(simulator, 250), ==, -1);
 	munit_assert_ptr_null(simulator->event_schedule);
 
-	simulator_destroy(simulator);
+	return MUNIT_OK;
+}
+
+MunitResult test_signal_writers(const MunitParameter params[], void *user_data_or_fixture) {
+
+	Simulator *simulator = (Simulator *) user_data_or_fixture;
+	munit_assert_ptr_not_null(simulator);
+
+	// setup
+	Signal s0 = signal_create(simulator->signal_pool);
+	Signal s1 = signal_create(simulator->signal_pool);
+
+	ChipDummy *c1 = chip_dummy_create(simulator, (Signal[CHIP_DUMMY_PIN_COUNT]) {
+										[CHIP_DUMMY_O0] = s0,
+										[CHIP_DUMMY_O1] = s1
+	});
+	ChipDummy *c2 = chip_dummy_create(simulator, (Signal[CHIP_DUMMY_PIN_COUNT]) {
+										[CHIP_DUMMY_O0] = s0,
+										[CHIP_DUMMY_O1] = s1
+	});
+	simulator_register_chip(simulator, (Chip *) c1, "C1");
+	simulator_register_chip(simulator, (Chip *) c2, "C2");
+	simulator_device_complete(simulator);
+
+	munit_assert_int32(c1->id, ==, 0);
+	munit_assert_int32(c2->id, ==, 1);
+
+	// no writers
+	signal_pool_cycle(simulator->signal_pool);
+	munit_assert_uint64(simulator_signal_writers(simulator, s0), ==, 0ull);
+	munit_assert_uint64(simulator_signal_writers(simulator, s1), ==, 0ull);
+
+	// one writer
+	signal_write(simulator->signal_pool, c1->signals[CHIP_DUMMY_O0], false);
+	signal_pool_cycle(simulator->signal_pool);
+	munit_assert_uint64(simulator_signal_writers(simulator, s0), ==, 0x1ull);
+	munit_assert_uint64(simulator_signal_writers(simulator, s1), ==, 0x0ull);
+
+	// two writers
+	signal_write(simulator->signal_pool, c2->signals[CHIP_DUMMY_O0], false);
+	signal_pool_cycle(simulator->signal_pool);
+	munit_assert_uint64(simulator_signal_writers(simulator, s0), ==, 0x3ull);
+	munit_assert_uint64(simulator_signal_writers(simulator, s1), ==, 0x0ull);
+
+	// back to one writer
+	signal_clear_writer(simulator->signal_pool, c1->signals[CHIP_DUMMY_O0]);
+	signal_pool_cycle(simulator->signal_pool);
+	munit_assert_uint64(simulator_signal_writers(simulator, s0), ==, 0x2ull);
+	munit_assert_uint64(simulator_signal_writers(simulator, s1), ==, 0x0ull);
+
+	// write another signal
+	signal_write(simulator->signal_pool, c1->signals[CHIP_DUMMY_O1], false);
+	signal_pool_cycle(simulator->signal_pool);
+	munit_assert_uint64(simulator_signal_writers(simulator, s0), ==, 0x2ull);
+	munit_assert_uint64(simulator_signal_writers(simulator, s1), ==, 0x1ull);
+
+	// stop writing
+	signal_clear_writer(simulator->signal_pool, c2->signals[CHIP_DUMMY_O0]);
+	signal_clear_writer(simulator->signal_pool, c1->signals[CHIP_DUMMY_O1]);
+	signal_pool_cycle(simulator->signal_pool);
+	munit_assert_uint64(simulator_signal_writers(simulator, s0), ==, 0ull);
+	munit_assert_uint64(simulator_signal_writers(simulator, s1), ==, 0ull);
+
 	return MUNIT_OK;
 }
 
 MunitTest simulator_tests[] = {
-	{ "/schedule_event", test_schedule_event, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
-	{ "/pop_event", test_pop_event, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL },
+	{ "/schedule_event", test_schedule_event, simulator_setup, simulator_teardown, MUNIT_TEST_OPTION_NONE, NULL },
+	{ "/pop_event", test_pop_event, simulator_setup, simulator_teardown, MUNIT_TEST_OPTION_NONE, NULL },
+	{ "/signal_writers", test_signal_writers, simulator_setup, simulator_teardown, MUNIT_TEST_OPTION_NONE, NULL },
 	{ NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL }
 };
