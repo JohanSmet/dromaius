d89689e3e4eecfee9cb641342e4bd34d41d6ad4e
Johan Smet <johan.smet@justcode.be>
2022-03-12 21:36:35 +0100

    htmlview: update wasm-server script to send correct MIME type headers.

--- src/htmlview/scripts/wasm-server.py
+++ src/htmlview/scripts/wasm-server.py
@@ -10,6 +10,13 @@ class WasmHandler(SimpleHTTPRequestHandler):
         self.send_header("Cross-Origin-Embedder-Policy", "require-corp")
         SimpleHTTPRequestHandler.end_headers(self)
 
+        WasmHandler.extensions_map['.js']   = 'application/x-javascript'
+        WasmHandler.extensions_map['.html'] = 'text/html',
+        WasmHandler.extensions_map['.png']  = 'image/png',
+        WasmHandler.extensions_map['.jpg']  = 'image/jpg',
+        WasmHandler.extensions_map['.svg']  = 'image/svg+xml',
+        WasmHandler.extensions_map['.css']  = 'text/css',
+
         if sys.version_info < (3, 7, 5):
             WasmHandler.extensions_map['.wasm'] = 'application/wasm'
 
